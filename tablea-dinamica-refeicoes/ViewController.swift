a7ad4c307930fc79e49f3adddfe83f2ef29c1141
Vinicius Sossei Sakugawa <viny.sossei@gmail.com>
2021-01-17 17:08:02 -0300

    section of course finalized

--- tablea-dinamica-refeicoes/ViewController.swift
+++ tablea-dinamica-refeicoes/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    let refeicoes = ["Churros", "Macarrão", "Pizza"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("tableviewcontroller viewDidLoad")
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        cell.textLabel?.text = refeicao
+        print(refeicao)
+        return cell
+    }
 }
 
